298b724b06838583c7a7f2014a44a41665791e8b
Samuel Daniel <sam@firstmode.com>
2024-08-27 02:31:15 -0700

    initiate full burn

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,13 @@
 #define NUM_LEDS 200
 #define NUM_ANTENNAE 4
 #define ONE_MIN_IN_MILLIS 60000
-#define CHOICES 9
+#define CHOICES 4
 #define PATTERN_CYCLE_TIME (ONE_MIN_IN_MILLIS * 1)
 
-CRGB strip_2[NUM_LEDS];
-CRGB strip_3[NUM_LEDS];
-CRGB strip_4[NUM_LEDS];
-CRGB strip_5[NUM_LEDS];
+CRGB strip_6[NUM_LEDS];
+CRGB strip_7[NUM_LEDS];
+CRGB strip_8[NUM_LEDS];
+CRGB strip_9[NUM_LEDS];
 CRGB *antennae[NUM_ANTENNAE];
 
 unsigned long last_millis;
@@ -27,18 +27,18 @@ extern TBlendType color_palette_blending;
 
 void setup() {
   delay(3000); // power-up safety delay
-  FastLED.addLeds<WS2811, 2, RGB>(strip_2, NUM_LEDS);
-  FastLED.addLeds<WS2811, 3, RGB>(strip_3, NUM_LEDS);
-  FastLED.addLeds<WS2811, 4, RGB>(strip_4, NUM_LEDS);
-  FastLED.addLeds<WS2811, 5, RGB>(strip_5, NUM_LEDS);
-  black_out(strip_2, NUM_LEDS);
-  black_out(strip_3, NUM_LEDS);
-  black_out(strip_4, NUM_LEDS);
-  black_out(strip_5, NUM_LEDS);
-  antennae[0] = strip_2;
-  antennae[1] = strip_3;
-  antennae[2] = strip_4;
-  antennae[3] = strip_5;
+  FastLED.addLeds<WS2811, 6, RGB>(strip_6, NUM_LEDS);
+  FastLED.addLeds<WS2811, 7, RGB>(strip_7, NUM_LEDS);
+  FastLED.addLeds<WS2811, 8, RGB>(strip_8, NUM_LEDS);
+  FastLED.addLeds<WS2811, 9, RGB>(strip_9, NUM_LEDS);
+  black_out(strip_6, NUM_LEDS);
+  black_out(strip_7, NUM_LEDS);
+  black_out(strip_8, NUM_LEDS);
+  black_out(strip_9, NUM_LEDS);
+  antennae[0] = strip_6;
+  antennae[1] = strip_7;
+  antennae[2] = strip_8;
+  antennae[3] = strip_9;
   last_millis = millis();
 
   // init a pattern for the first five mins
@@ -50,47 +50,26 @@ void setup() {
 void loop() {
   unsigned long now = millis();
   if (now - last_millis >= PATTERN_CYCLE_TIME) {
-    // int choice = random(0, CHOICES + 1);
-    int choice = current_choice;
-    if (0 == choice) {
+    current_choice = (current_choice + 1) % CHOICES;
+    last_millis = now;
+    if (0 == current_choice) {
       color_palette_palette = RainbowColors_p;
       color_palette_blending = LINEARBLEND;
       current_loop = color_palette_loop;
     }
-    if (1 == choice) {
+    if (1 == current_choice) {
       color_palette_palette = RainbowStripeColors_p;
-      color_palette_blending = LINEARBLEND;
+      color_palette_blending = NOBLEND;
       current_loop = color_palette_loop;
     }
-    if (2 == choice) {
-      color_palette_setup_random();
-      color_palette_blending = LINEARBLEND;
-      current_loop = color_palette_loop;
-    }
-    if (3 == choice) {
-      color_palette_palette = CloudColors_p;
-      color_palette_blending = LINEARBLEND;
-      current_loop = color_palette_loop;
-    }
-    if (4 == choice) {
+    if (2 == current_choice) {
       color_palette_palette = PartyColors_p;
-      color_palette_blending = LINEARBLEND;
+      color_palette_blending = NOBLEND;
       current_loop = color_palette_loop;
     }
-    if (5 == choice) {
-      current_loop = cylon_loop;
-    }
-    if (6 == choice) {
+    if (3 == current_choice) {
       current_loop = rainbow_cylon_loop;
     }
-    if (7 == choice) {
-      current_loop = pacifica_loop;
-    }
-    if (8 == choice) {
-      current_loop = sinc_loop;
-    }
-    current_choice = (current_choice + 1) % CHOICES;
   }
   current_loop(antennae, NUM_ANTENNAE, NUM_LEDS);
-  last_millis = now;
 }
